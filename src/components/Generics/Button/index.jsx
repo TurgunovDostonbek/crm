87f12ece2addc456fdc40a046509f6a64a5ac8e5
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2024-10-09 17:49:59 +0500

    Project Configuration

---
+++ src/components/Generics/Button/index.jsx
@@ -0,0 +1,5 @@
+import { Container } from "../Sidebar/style";
+
+export const Navbar = () => {
+  return <Container>Navbar</Container>;
+};
---
+++ src/root/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from "../components/Sidebar/style";
+
+function Root() {
+  return (
+    <Container>
+      <h1>Crm Projects</h1>
+    </Container>
+  );
+}
+
+export default Root;
